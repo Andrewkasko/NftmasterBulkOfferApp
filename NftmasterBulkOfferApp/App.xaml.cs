39c05d87819cb215fc3c8b1dc727530c40e97935
Andrew Kaskaniotis <akaskaniotis@gmail.com>
2022-09-01 17:13:53 +1000

    Add project files.

---
+++ NftmasterBulkOfferApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NftmasterBulkOfferApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NftmasterBulkOfferApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NftmasterBulkOfferApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
